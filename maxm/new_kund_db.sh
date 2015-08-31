c1e25f280a9ccd76c4648c148a0a21ff46353251
Simon Wikander <simon.wikander@maxm.se>
2015-08-31 11:57:21 +0200

    Last on maxm.

--- maxm/new_kund_db.sh
+++ maxm/new_kund_db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 MAXCUST_PATH="/Users/simonw/projects/git/maxcust"
 SCHEMA_PATH="$MAXCUST_PATH/schema"
-pushd $SCHEMA_PATH 
- psql -c "drop database kund"
-psql -f database_mac.sql
+pushd $SCHEMA_PATH
+psql -c "drop database kund"
+psql -f database.sql
 psql kund -f schema.sql
 psql kund -f tables.sql
+psql kund -f behorighet_tables.sql
+psql kund -f importinfo_tables.sql
+psql kund -f synch_tables.sql
+psql kund -f schema_checksum_table.sql
 psql kund -f base_data.sql
 psql kund -f example_data.sql
 popd
-
